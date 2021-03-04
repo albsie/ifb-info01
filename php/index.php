95ff90db7fc3244d576489f67fc31af99cca34ec
Sieghard Alber <albsie@DESKTOP-MQBVCS7.localdomain>
2021-03-04 08:57:31 +0100

    feat: init with js

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<?php 
+$name = "Sigi";
+$klasse = "INFO 1";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>php test</title>
+</head>
+
+<body>
+    <div>
+        Hallo <?=$klasse?> hier ist <?=$name?>, wie geht es euch?
+
+    </div>
+</body>
+
+</html>
