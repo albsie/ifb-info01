ba4b8435bd0fd3abe3cc5f3abe86e3f9b97e9dca
albsie <albsieweb@gmail.com>
2021-04-22 10:22:58 +0200

    updated form

--- javaScript/exercieses/formvaliedierung/main.js
+++ javaScript/exercieses/formvaliedierung/main.js
@@ -2,4 +2,20 @@
 
 const form = document.getElementById('form');
 
-console.log(form);
+form.submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    /*
+    Vorname Validierung
+    */
+    if (form.firstname.value.length <= 2 || form.firstname.value.length >= 10) {
+        form.firstname.classList.add('is-invalid');
+        form.firstname.classList.remove('is-valid');
+        form.firstname.nextElementSibling.classList.add('invalid-feedback');
+        form.firstname.nextElementSibling.classList.remove('valid-feedback');
+        form.firstname.nextElementSibling.text = "Die Zeichenlänge ist nicht korrekt";
+        console.log(form.firstname.nextElementSibling);
+    } else {
+        form.firstname.classList.add('is-valid');
+        form.firstname.classList.remove('is-invalid');
+    }
+});
