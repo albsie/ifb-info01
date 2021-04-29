b66a3af174fdec0e74e7b35ec808cee241686a83
albsie <albsieweb@gmail.com>
2021-04-29 10:16:36 +0200

    feat: email validator

--- javaScript/exercieses/formvaliedierung/main.js
+++ javaScript/exercieses/formvaliedierung/main.js
@@ -25,21 +25,82 @@ form.submitBtn.addEventListener('click', (event) => {
     Nachname Validierung
     Aufgabe: Der Nachname soll eine Länge von 2 bis 50 Zeichen haben.
     */
-
+    if (form.lastname.value.length <= 2 || form.lastname.value.length >= 50) {
+        errors++;
+        form.lastname.classList.add('is-invalid');
+        form.lastname.classList.remove('is-valid');
+        form.lastname.nextElementSibling.classList.add('invalid-feedback');
+        form.lastname.nextElementSibling.innerText = "Die Zeichenlänge ist nicht korrekt";
+    } else {
+        form.lastname.classList.add('is-valid');
+        form.lastname.classList.remove('is-invalid');
+        form.lastname.nextElementSibling.classList.remove('invalid-feedback');
+        form.lastname.nextElementSibling.innerText = "";
+    }
     /*
     Passwort Valiedierung
     Aufgabe: Das Passwort soll eine Länge von 8 bis 50 Zeichen haben.
     */
+    if (form.password.value.length <= 8 || form.password.value.length >= 50) {
+        errors++;
+        form.password.classList.add('is-invalid');
+        form.password.classList.remove('is-valid');
+        form.password.nextElementSibling.classList.add('invalid-feedback');
+        form.password.nextElementSibling.innerText = "Die Passwortlänge ist nicht korrekt";
+        form.passwordRpt.classList.remove('is-invalid');
+        form.passwordRpt.classList.remove('is-valid');
+        form.passwordRpt.nextElementSibling.classList.remove('invalid-feedback');
+        form.passwordRpt.nextElementSibling.innerText = "";
+    } else {
+        form.password.classList.add('is-valid');
+        form.password.classList.remove('is-invalid');
+        form.password.nextElementSibling.classList.remove('invalid-feedback');
+        form.password.nextElementSibling.innerText = "";
 
-    /*
-    Passwort Wiederholung Valiedierung
-    Aufgabe: Die Passwort Wiederholung muss mit dem Passwort identisch sein.
-    */
+        /*
+        Passwort Wiederholung Valiedierung
+        Aufgabe: Die Passwort Wiederholung muss mit dem Passwort identisch sein.
+        */
+
+        if(form.passwordRpt.value !== form.password.value){
+          errors++;
+          form.passwordRpt.classList.add('is-invalid');
+          form.passwordRpt.classList.remove('is-valid');
+          form.passwordRpt.nextElementSibling.classList.add('invalid-feedback');
+          form.passwordRpt.nextElementSibling.innerText = "Die Passwort Wiederholung ist nicht korrekt";
+        } else {
+          form.passwordRpt.classList.add('is-valid');
+          form.passwordRpt.classList.remove('is-invalid');
+          form.passwordRpt.nextElementSibling.classList.remove('invalid-feedback');
+          form.passwordRpt.nextElementSibling.innerText = "";
+        }
+    }
+
+
 
     /*
     Email Valiedierung
-    Aufgabe: die Email muss ein @ und ein Punkt enthalten. 
+    Aufgabe: die Email muss ein @ und ein Punkt enthalten.
     */
+    if(!emailIsValid(form.email.value)){
+      errors++;
+      form.email.classList.add('is-invalid');
+      form.email.classList.remove('is-valid');
+      form.email.nextElementSibling.classList.add('invalid-feedback');
+      form.email.nextElementSibling.innerText = "Die Email Adresse ist nicht korrekt";
+    } else {
+      form.email.classList.add('is-valid');
+      form.email.classList.remove('is-invalid');
+      form.email.nextElementSibling.classList.remove('invalid-feedback');
+      form.email.nextElementSibling.innerText = "";
+    }
+
+    /*
+    Selectfeld Validierung
+    Aufgabe: 1,2 oder 3 muss ausgewählt sein.
+    */
+
+    console.log(form.country.value);
 
     /*
     Wenn es keine Fehler mehr gibt, dann lade die info.html
@@ -48,3 +109,10 @@ form.submitBtn.addEventListener('click', (event) => {
       window.location.href = "info.html";
     }
 });
+
+/*
+Email Validation Regex
+*/
+function emailIsValid (email) {
+  return /\S+@\S+\.\S+/.test(email)
+}
