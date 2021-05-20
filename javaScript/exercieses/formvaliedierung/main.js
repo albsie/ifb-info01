1b8bbdf2aba09c5a43a0e475da581ec704aac1f7
albsie <albsieweb@gmail.com>
2021-05-20 09:36:55 +0200

    feat: new toggelmenu

--- javaScript/exercieses/formvaliedierung/main.js
+++ javaScript/exercieses/formvaliedierung/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const form = document.getElementById('form');
+const form = document.getElementById('form'),
+    radioError = document.getElementById('radioErrorText');
 
 form.submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
@@ -62,17 +63,17 @@ form.submitBtn.addEventListener('click', (event) => {
         Aufgabe: Die Passwort Wiederholung muss mit dem Passwort identisch sein.
         */
 
-        if(form.passwordRpt.value !== form.password.value){
-          errors++;
-          form.passwordRpt.classList.add('is-invalid');
-          form.passwordRpt.classList.remove('is-valid');
-          form.passwordRpt.nextElementSibling.classList.add('invalid-feedback');
-          form.passwordRpt.nextElementSibling.innerText = "Die Passwort Wiederholung ist nicht korrekt";
+        if (form.passwordRpt.value !== form.password.value) {
+            errors++;
+            form.passwordRpt.classList.add('is-invalid');
+            form.passwordRpt.classList.remove('is-valid');
+            form.passwordRpt.nextElementSibling.classList.add('invalid-feedback');
+            form.passwordRpt.nextElementSibling.innerText = "Die Passwort Wiederholung ist nicht korrekt";
         } else {
-          form.passwordRpt.classList.add('is-valid');
-          form.passwordRpt.classList.remove('is-invalid');
-          form.passwordRpt.nextElementSibling.classList.remove('invalid-feedback');
-          form.passwordRpt.nextElementSibling.innerText = "";
+            form.passwordRpt.classList.add('is-valid');
+            form.passwordRpt.classList.remove('is-invalid');
+            form.passwordRpt.nextElementSibling.classList.remove('invalid-feedback');
+            form.passwordRpt.nextElementSibling.innerText = "";
         }
     }
 
@@ -82,37 +83,101 @@ form.submitBtn.addEventListener('click', (event) => {
     Email Valiedierung
     Aufgabe: die Email muss ein @ und ein Punkt enthalten.
     */
-    if(!emailIsValid(form.email.value)){
-      errors++;
-      form.email.classList.add('is-invalid');
-      form.email.classList.remove('is-valid');
-      form.email.nextElementSibling.classList.add('invalid-feedback');
-      form.email.nextElementSibling.innerText = "Die Email Adresse ist nicht korrekt";
+    if (!emailIsValid(form.email.value)) {
+        errors++;
+        form.email.classList.add('is-invalid');
+        form.email.classList.remove('is-valid');
+        form.email.nextElementSibling.classList.add('invalid-feedback');
+        form.email.nextElementSibling.innerText = "Die Email Adresse ist nicht korrekt";
     } else {
-      form.email.classList.add('is-valid');
-      form.email.classList.remove('is-invalid');
-      form.email.nextElementSibling.classList.remove('invalid-feedback');
-      form.email.nextElementSibling.innerText = "";
+        form.email.classList.add('is-valid');
+        form.email.classList.remove('is-invalid');
+        form.email.nextElementSibling.classList.remove('invalid-feedback');
+        form.email.nextElementSibling.innerText = "";
     }
 
     /*
     Selectfeld Validierung
     Aufgabe: 1,2 oder 3 muss ausgewählt sein.
     */
+    if (form.country.value === "0") {
+        errors++;
+        form.country.classList.add('is-invalid');
+        form.country.classList.remove('is-valid');
+        form.country.nextElementSibling.classList.add('invalid-feedback');
+        form.country.nextElementSibling.innerText = "Bitte wählen Sie ein Land";
+    } else {
+        form.country.classList.add('is-valid');
+        form.country.classList.remove('is-invalid');
+        form.country.nextElementSibling.classList.remove('invalid-feedback');
+        form.country.nextElementSibling.innerText = "";
+    }
+    /*
+    Validierung vom Alter
+    Aufgabe: schmeiß einen Fehler wenn kleiner als 18.
+    */
+    if (form.age.value >= 18) {
+        form.age.classList.add('is-valid');
+        form.age.classList.remove('is-invalid');
+        form.age.nextElementSibling.classList.remove('invalid-feedback');
+        form.age.nextElementSibling.innerText = "";
+    } else {
+        errors++;
+        form.age.classList.add('is-invalid');
+        form.age.classList.remove('is-valid');
+        form.age.nextElementSibling.classList.add('invalid-feedback');
+        form.age.nextElementSibling.innerText = "Du musst 18 Jahre alt sein.";
+    }
+    /*
+    Valiedierung Radio Buttons: Ein Feld muss ausgewählt sein.
+    */
 
-    console.log(form.country.value);
+    if (form.gender.value === "1" || form.gender.value === "2" || form.gender.value === "3") {
+        //form.gender.classList.add('is-valid');
+        //form.gender.classList.remove('is-invalid');
+        radioError.innerText = "";
+        radioError.classList.remove('invalid-feedback');
+        for (let label of form.gender) {
+            label.nextElementSibling.style.color = "#212529";
+        }
+        form.gender[form.gender.value - 1].nextElementSibling.style.color = "#198754";
+    } else {
+        errors++;
+        //form.gender.classList.add('is-invalid');
+        //form.gender.classList.remove('is-valid');
+        radioError.innerText = "Wählen Sie Ihr Geschlecht";
+        radioError.classList.add('invalid-feedback');
+        radioError.style.display = "block";
+        //for (let i = 0; i < form.gender.length; i++) {
+        //  form.gender[i].nextElementSibling.style.color = "#212529";
+        //}
+        for (let label of form.gender) {
+            label.nextElementSibling.style.color = "#dc3545";
+        }
+    }
+
+    /*
+    Validierung Checkbox: Muss ausgewählt sein.    
+    */
+    console.log(form.chkbox.checked);
+    if (form.chkbox.checked) {
+        form.chkbox.nextElementSibling.style.color = "#198754";
+    } else {
+        errors++;
+        form.chkbox.nextElementSibling.style.color = "#dc3545";
+    }
 
     /*
     Wenn es keine Fehler mehr gibt, dann lade die info.html
     */
-    if(errors === 0){
-      window.location.href = "info.html";
+    if (errors === 0) {
+        window.location.href = "info.html";
     }
 });
 
 /*
 Email Validation Regex
 */
-function emailIsValid (email) {
-  return /\S+@\S+\.\S+/.test(email)
+function emailIsValid(email) {
+    return /\S+@\S+\.\S+/.test(email)
 }
